e5f18595213b8b161d324756777a7d5d75624c7d
Zelefant <zelefantplaysmc@outlook.com>
2026-07-16 13:34:45 -0500

    Function definitions for bsp renderer

--- bsp/BSPRenderer.h
+++ bsp/BSPRenderer.h
@@ -1,8 +1,21 @@
 #ifndef BSP_RENDERER_CLASS_H
 #define BSP_RENDERER_CLASS_H
 
+#include "BSPTree.h"
+#include <GLFW/glfw3.h>
+
 class BSPRenderer
 {
+public:
+	BSPRenderer(GLFWwindow* window, BSPTree tree);
+
+	void RenderFromLocation(glm::vec2 playerPosition);
+
+
+private:
+
+	GLFWwindow* window;
+	BSPTree tree;
 
 };
 
